3dfee30d81664383de8eb6c5b392c8cd9f94042c
Rekapost <104472499+Rekapost@users.noreply.github.com>
2025-06-11 11:43:51 -0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from ragas.llms import LangchainLLMWrapper
 @pytest.fixture()
 def llm_wrapper():
     os.environ[
-        "OPENAI_API_KEY"] = "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"
+        "OPENAI_API_KEY"] = "sk-proj-csXhU7U6E19NyCgGGbfAKy4VsoHZIq9WxHRe88NoxFbhTFscTu2or7pzFJxTYA"
     api_key = os.environ["OPENAI_API_KEY"]
     llm = ChatOpenAI(model="gpt-3.5-turbo",
                      temperature=0,
